462b646b6b6f305a1fb30d5d6ca023c1faee42c8
Ravbug <ravbuganimations@gmail.com>
2020-04-07 22:55:13 -0700

    Fix highlighting issues InteractableObject.cpp ~ optimization - disable ticking every frame
    OutlineMat.uasset
    ~ change default Width to 1.0 from 0.1 (now visible)

--- Source/VRHeightSimulator/InteractableObject.cpp
+++ Source/VRHeightSimulator/InteractableObject.cpp
@@ -7,7 +7,7 @@
 AInteractableObject::AInteractableObject()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//PrimaryActorTick.bCanEverTick = true;
 
 	rootMesh = CreateDefaultSubobject<UStaticMeshComponent>("Root Mesh");
 	RootComponent = rootMesh;
